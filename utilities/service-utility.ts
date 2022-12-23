c19adca88e7345362506b6095088ca3ca69884a7
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2022-12-24 00:29:26 +0800

    Add handling for invalid password

--- utilities/service-utility.ts
+++ utilities/service-utility.ts
@@ -6,7 +6,8 @@ type FirebaseError = {
 export const getErrorMessage = (error: FirebaseError) => {
   switch (error.message) {
     case "EMAIL_NOT_FOUND":
-      return "Invalid sign in account!";
+    case "INVALID_PASSWORD":
+      return "Invalid sign-in account!";
     default:
       return error.message;
   }
