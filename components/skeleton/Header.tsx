40fd14e871de0da29cf1e378cd14f9682e927c28
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2022-12-24 01:04:03 +0800

    Display brand and sign-out button in header

--- components/skeleton/Header.tsx
+++ components/skeleton/Header.tsx
@@ -1,8 +1,41 @@
-import { Layout } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { Layout, Menu, MenuProps } from "antd";
+import Link from "next/link";
+import { FC, useContext } from "react";
+import { AuthContext } from "../../store/auth-context";
 import classes from "./Header.module.css";
 
-const Header: React.FC = () => {
-  return <Layout.Header className={classes.header}></Layout.Header>;
+const Header: FC = () => {
+  const { user, signOut } = useContext(AuthContext);
+
+  const items: MenuProps["items"] = [
+    {
+      key: "USER",
+      label: user?.email,
+      icon: <UserOutlined />,
+      children: [{ key: "SIGN_OUT", label: "Sign Out" }],
+    },
+  ];
+
+  const handleClick: MenuProps["onClick"] = ({ key }) => {
+    if (key !== "SIGN_OUT") return;
+
+    signOut();
+  };
+
+  return (
+    <Layout.Header className={classes.header}>
+      <Link className={classes.brand} href="/">
+        EFP
+      </Link>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        items={items}
+        onClick={handleClick}
+      />
+    </Layout.Header>
+  );
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,17 @@
-import { Button } from "antd";
 import Head from "next/head";
-import { useContext } from "react";
 import AuthenticatedLayout from "../components/layout/AuthenticatedLayout";
 import useAuthGuard from "../hooks/useAuthGuard";
-import { AuthContext, AuthContextProvider } from "../store/auth-context";
+import { AuthContextProvider } from "../store/auth-context";
 import { NextPageWithLayout } from "./_app";
 
 const Home: NextPageWithLayout = () => {
   useAuthGuard();
-
-  const { signOut } = useContext(AuthContext);
-
-  const handleSignOut = () => {
-    signOut();
-  };
 
   return (
     <>
       <Head>
         <title>Enterprise Forms Platform</title>
       </Head>
-      <Button onClick={handleSignOut}>Sign Out</Button>
     </>
   );
 };
