6f2a9d78b35f165ef977d699fdd2c14f5e0fa4d7
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2022-12-30 23:46:30 +0800

    Use form components in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,65 @@
+import { Space, Spin } from "antd";
+import axios from "axios";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import FormGroups from "../components/forms/FormGroups";
 import AuthenticatedLayout from "../components/layout/AuthenticatedLayout";
 import useAuthGuard from "../hooks/useAuthGuard";
+import * as formGroupService from "../services/form-groups-service";
+import * as formService from "../services/forms-service";
 import { AuthContextProvider } from "../store/auth-context";
+import FormGroupType from "../types/form-group-type";
+import FormType from "../types/form-type";
 import { NextPageWithLayout } from "./_app";
 
 const Home: NextPageWithLayout = () => {
   useAuthGuard();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [formGroups, setFormGroups] = useState<FormGroupType[]>([]);
+  const [forms, setForms] = useState<FormType[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .all([formGroupService.findAll(), formService.findAll()])
+      .then(
+        axios.spread((...responses) => {
+          const formGroupsData = responses[0].data;
+          setFormGroups(
+            Object.keys(formGroupsData).map((id) => ({
+              id,
+              name: formGroupsData[id].name,
+            }))
+          );
+
+          const formsData = responses[1].data;
+          setForms(
+            Object.keys(formsData).map((id) => ({
+              id,
+              groupId: formsData[id].groupId,
+              name: formsData[id].name,
+            }))
+          );
+        })
+      )
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
       <Head>
         <title>Enterprise Forms Platform</title>
       </Head>
+      <Spin spinning={loading} />
+      {!loading && (
+        <Space direction="vertical" style={{ width: "100%" }}>
+          <FormGroups formGroups={formGroups} forms={forms} />
+        </Space>
+      )}
     </>
   );
 };
