98ec82ad78c8793be83a478001855e4968521bad
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2023-01-02 00:11:14 +0800

    Make footer year dynamic

--- components/skeleton/Footer.tsx
+++ components/skeleton/Footer.tsx
@@ -1,13 +1,16 @@
 import { GithubOutlined } from "@ant-design/icons";
 import { Layout } from "antd";
+import moment from "moment";
 import Link from "next/link";
 import classes from "./Footer.module.css";
 
 const Footer: React.FC = () => {
+  const year = moment().format("yyyy");
+
   return (
     <Layout.Footer className={classes.footer}>
       <small>
-        © Copyright 2022{" "}
+        © Copyright {`${year} `}
         <Link href="https://github.com/pandenutella" target="_blank">
           <GithubOutlined /> Pan de Nutella
         </Link>
